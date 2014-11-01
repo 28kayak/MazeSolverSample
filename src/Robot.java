65215d0611994f14377b945f3e120628a9796132
28kayak <kaya_ota@msn.com>
2014-11-01 01:51:57 -0700

    add a function

--- src/Robot.java
+++ src/Robot.java
@@ -2,12 +2,19 @@
 public class Robot 
 {
 	enum directions{front, right, left, back,}
+	//enum wall{oepned, closed,}
 	
 	int getCurrentRoomID(int current_x, int current_y)
 	{
 		int current_id;
 		current_id = current_x + 4*current_y;
 		return current_id;
+	}
+	boolean[] getSencerValue()
+	{
+		boolean sencerResult[] = new boolean[3];
+		//if door is opened, hold value is ture; otherwise, false.
+		return sencerResult;
 	}
 	
 	
